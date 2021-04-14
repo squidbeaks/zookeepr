4da934d3585a9f5702fc016bf0278957d371a592
Leah Russell <leahsigridrussell@gmail.com>
2021-04-14 08:23:59 -0700

    making port an env var

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { animals } = require('./data/animals');
 
+const PORT = procore.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -43,6 +44,6 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
